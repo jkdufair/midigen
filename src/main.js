3713aec4b923a1a63edfb1fbffa1ab1d902cbb8e
Jason Dufair <jase@dufair.org>
2025-01-15 22:45:28 -0500

    Helix/loopy updates

--- src/main.js
+++ src/main.js
@@ -29,19 +29,16 @@ const logger = winston.createLogger({
 
 //#region Constants
 
-const vl3Channel = 1
-const vl3Harmony = 110
-const vl3Double = 111
+const harmonyChannel = 1
+const harmonyVoice1Controller = 110
+const harmonyVoice2Controller = 111
 
-const hd500xChannel = 2
-const hd500xFootswitch1 = 51
-const hd500xFootswitch2 = 52
-const hd500xFootswitch3 = 53
-const hd500xFootswitch4 = 54
+const helixChannel = 2
+const helixSnapshot = 69
 
-const rc5Channel = 3
-const rc5RecPlay = 80
-const rc5Clear = 82
+const loopyChannel = 3
+const loopyGuitarController = 80
+const loopyVocalController = 86
 
 const onSongChannel = 4
 const onSongNextSection = 1
@@ -54,12 +51,6 @@ const off = 0
 //#endregion Constants
 
 //#region Support Functions
-
-const hd500xProgram = (programPreset) => {
-	const [_, bank, preset] = programPreset.split(/(\d+)/)
-	const offset = preset.charCodeAt(0) - 65 + 1
-	return (parseInt(bank) - 1) * 4 + offset - 1
-}
 
 const ticksFromLength = (length, beatsPerMeasure) => {
 	const [measures, beats, subdivisions] = length.split('.').map(x => parseInt(x))
@@ -83,135 +74,80 @@ const eventFromName = (eventName, delta, parameter) => {
 	switch (eventName) {
 		case 'harmony-on':
 			return new ControllerChangeEvent({
-				controllerNumber: vl3Harmony,
+				controllerNumber: harmonyVoice1Controller,
 				controllerValue: on,
-				channel: vl3Channel,
+				channel: harmonyChannel,
 				delta: delta
 			})
 		case 'harmony-off':
 			return new ControllerChangeEvent({
-				controllerNumber: vl3Harmony,
+				controllerNumber: harmonyVoice1Controller,
 				controllerValue: off,
-				channel: vl3Channel,
+				channel: harmonyChannel,
 				delta: delta
 			})
 		case 'vocal-double-on':
 			return new ControllerChangeEvent({
-				controllerNumber: vl3Double,
+				controllerNumber: harmonyVoice2Controller,
 				controllerValue: on,
-				channel: vl3Channel,
+				channel: harmonyChannel,
 				delta: delta
 			})
 		case 'vocal-double-off':
 			return new ControllerChangeEvent({
-				controllerNumber: vl3Double,
+				controllerNumber: harmonyVoice2Controller,
 				controllerValue: off,
-				channel: vl3Channel,
+				channel: harmonyChannel,
 				delta: delta
 			})
-		case 'vl3-patch-change':
+		case 'vocal-patch-change':
 			return new ProgramChangeEvent({
-				channel: vl3Channel - 1,
+				channel: harmonyChannel - 1,
 				instrument: parameter - 1,
 				delta: delta
 			})
+		case 'vocal-loop-record':
+			return new ControllerChangeEvent({
+				controllerNumber: loopyVocalController + parameter - 1, controllerValue: on, channel: loopyChannel, delta: delta
+			})
+		case 'vocal-loop-play':
+			return new ControllerChangeEvent({
+				controllerNumber: loopyVocalController + parameter - 1, controllerValue: off, channel: loopyChannel, delta: delta
+			})
+		case 'vocal-loop-stop':
+			return new ControllerChangeEvent({
+				controllerNumber: loopyVocalController + parameter, controllerValue: off, channel: loopyChannel, delta: delta
+			})
+		case 'vocal-loop-clear':
+			return new ControllerChangeEvent({
+				controllerNumber: loopyVocalController + parameter, controllerValue: on, channel: loopyChannel, delta: delta
+			})
 
-		case 'hd500x-fs1-on':
-			return new ControllerChangeEvent({
-				controllerNumber: hd500xFootswitch1,
-				controllerValue: on,
-				channel: hd500xChannel,
-				delta: delta
-			})
-		case 'hd500x-fs1-off':
-			return new ControllerChangeEvent({
-				controllerNumber: hd500xFootswitch1,
-				controllerValue: off,
-				channel: hd500xChannel,
-				delta: delta
-			})
-		case 'hd500x-fs2-on':
-			return new ControllerChangeEvent({
-				controllerNumber: hd500xFootswitch2,
-				controllerValue: on,
-				channel: hd500xChannel,
-				delta: delta
-			})
-		case 'hd500x-fs2-off':
-			return new ControllerChangeEvent({
-				controllerNumber: hd500xFootswitch2,
-				controllerValue: off,
-				channel: hd500xChannel,
-				delta: delta
-			})
-		case 'hd500x-fs3-on':
-			return new ControllerChangeEvent({
-				controllerNumber: hd500xFootswitch3,
-				controllerValue: on,
-				channel: hd500xChannel,
-				delta: delta
-			})
-		case 'hd500x-fs3-off':
-			return new ControllerChangeEvent({
-				controllerNumber: hd500xFootswitch3,
-				controllerValue: off,
-				channel: hd500xChannel,
-				delta: delta
-			})
-		case 'hd500x-fs4-on':
-			return new ControllerChangeEvent({
-				controllerNumber: hd500xFootswitch4,
-				controllerValue: on,
-				channel: hd500xChannel,
-				delta: delta
-			})
-		case 'hd500x-fs4-off':
-			return new ControllerChangeEvent({
-				controllerNumber: hd500xFootswitch4,
-				controllerValue: off,
-				channel: hd500xChannel,
-				delta: delta
-			})
-		case 'hd500x-patch-change':
-			return [
-				new ControllerChangeEvent({channel: hd500xChannel, controllerNumber: 0, controllerValue: 0, delta: delta}),
-				new ControllerChangeEvent({channel: hd500xChannel, controllerNumber: 32, controllerValue: 6, delta: 0}),
-				new ProgramChangeEvent({channel: hd500xChannel - 1, instrument: hd500xProgram(parameter), delta: 0})
-			]
 
-		case 'rc5-rec-play':
-			return [
-				new ControllerChangeEvent({
-					controllerNumber: rc5RecPlay, controllerValue: on, channel: rc5Channel, delta: delta
-				}),
-				new ControllerChangeEvent({
-					controllerNumber: rc5RecPlay, controllerValue: off, channel: rc5Channel, delta: 0
+		case 'helix-snapshot':
+			return new ControllerChangeEvent({
+				controllerNumber: helixSnapshot,
+				controllerValue: parameter - 1,
+				channel: helixChannel,
+				delta: delta
+			})
+
+		case 'guitar-loop-record':
+			return new ControllerChangeEvent({
+					controllerNumber: loopyGuitarController + parameter - 1, controllerValue: on, channel: loopyChannel, delta: delta
 				})
-			]
-		case 'rc5-stop':
-			return [
-				new ControllerChangeEvent({
-					controllerNumber: rc5RecPlay, controllerValue: on, channel: rc5Channel, delta: delta
-				}),
-				new ControllerChangeEvent({
-					controllerNumber: rc5RecPlay, controllerValue: off, channel: rc5Channel, delta: 0
-				}),
-				new ControllerChangeEvent({
-					controllerNumber: rc5RecPlay, controllerValue: on, channel: rc5Channel, delta: 0
-				}),
-				new ControllerChangeEvent({
-					controllerNumber: rc5RecPlay, controllerValue: off, channel: rc5Channel, delta: 0
+		case 'guitar-loop-play':
+			return new ControllerChangeEvent({
+					controllerNumber: loopyGuitarController + parameter - 1, controllerValue: off, channel: loopyChannel, delta: delta
 				})
-			]
-		case 'rc5-clear':
-			return [
-				new ControllerChangeEvent({
-					controllerNumber: rc5Clear, controllerValue: on, channel: rc5Channel, delta: delta
-				}),
-				new ControllerChangeEvent({
-					controllerNumber: rc5Clear, controllerValue: off, channel: rc5Channel, delta: 0
+		case 'guitar-loop-stop':
+			return new ControllerChangeEvent({
+					controllerNumber: loopyGuitarController + parameter, controllerValue: off, channel: loopyChannel, delta: delta
 				})
-			]
+		case 'guitar-loop-clear':
+			return new ControllerChangeEvent({
+				controllerNumber: loopyGuitarController + parameter, controllerValue: on, channel: loopyChannel, delta: delta
+			})
 
 		case 'metronome-start':
 			return new ControllerChangeEvent({
@@ -228,6 +164,8 @@ const eventFromName = (eventName, delta, parameter) => {
 				channel: onSongChannel,
 				delta: delta
 			})
+		default:
+			console.log(`Unknown event: ${eventName}`)
 	}
 }
 
@@ -245,7 +183,6 @@ const writeSpecEvent = (event, delta, track) => {
 
 //#endregion Support Functions
 
-
 let track = new Track()
 
 // Get the spec file
@@ -260,17 +197,6 @@ logger.info(`${spec.title} (${spec.tempo} BPM)`)
 const [beatsPerMeasure, timeDivision] = spec.timeSignature.split('/').map(x => parseInt(x))
 track.setTimeSignature(beatsPerMeasure, timeDivision)
 track.setTempo(spec.tempo)
-
-// Initial program change for VL3 and HD500X
-track.addEvent(new ProgramChangeEvent({channel: vl3Channel - 1, instrument: spec.vl3Program - 1}))
-
-// "Main" bank on HD500X TODO: Make the setlist configurable
-track.addEvent(new ControllerChangeEvent({channel: hd500xChannel, controllerNumber: 0, controllerValue: 0, delta: 0}))
-track.addEvent(new ControllerChangeEvent({channel: hd500xChannel, controllerNumber: 32, controllerValue: 6, delta: 0}))
-track.addEvent(new ProgramChangeEvent({channel: hd500xChannel - 1, instrument: hd500xProgram(spec.hd500xProgram), delta: 0}))
-
-// Reset RC-5
-track.addEvent(eventFromName('rc5-clear', 0))
 
 // Set delta to the start of the first measure (count off) TODO: Make this configurable
 let nextEventDelta = Utils.getTickDuration(['1'], beatsPerMeasure)
@@ -340,15 +266,12 @@ spec.sections.forEach(section => {
 // Stop the metronome
 track.addEvent(eventFromName('metronome-stop', nextEventDelta))
 
-// Stop the RC-5
-track.addEvent(eventFromName('rc5-clear', 0))
-
 const write = new Writer(track)
 
 logger.verbose(JSON.stringify(track, null, 4)
                 .replaceAll('"channel": 0', '---VL3---')
-                .replaceAll('"channel": 1', '---HD500X---')
-                .replaceAll('"channel": 2', '---RC5---')
+                .replaceAll('"channel": 1', '---Helix---')
+                .replaceAll('"channel": 2', '---Guitar1 Loop---')
                 .replaceAll('"channel": 3', '---OnSong---')
                 .replaceAll(/\"status.*?,[\s\S]/g, '')
                 .replaceAll(/\"data.*?\[[\s\S]*?\]/g, '')
